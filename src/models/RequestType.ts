c770cf6cc408d8f390a708938383c8eb9a58d201
Deardavi01 <136112767+Deardavi01@users.noreply.github.com>
2024-03-08 16:17:57 -0400

    Crud sin probar
    modulo requestype con sus routes, controllers y validators

--- src/models/RequestType.ts
+++ src/models/RequestType.ts
@@ -16,6 +16,7 @@ RequestTypeModel.init(
         id: {
             type: DataTypes.INTEGER,
             primaryKey: true,
+            autoIncrement: true
         },
         description: {
             type: DataTypes.STRING,
---
+++ src/routes/requestTypes.ts
@@ -0,0 +1,28 @@
+import express from 'express';
+import {getAllRequestType,getRequestTypeById,createRequestType,updateRequestType} from '../controllers/RequestType';
+import RequestTypeValidator, { getRequestTypetByIdValidator } from '../validators/RequestType';
+const router = express.Router();
+
+//get all RequestTypes from the Account table
+router.get("/",
+            getAllRequestType);
+
+//get an a specific RequestType from the Account table
+router.get("/getRequestTypeById/:id",
+            getRequestTypetByIdValidator,
+            getRequestTypeById);
+
+//create a new RequestType in the Account table
+router.post("/createRequestType", 
+            RequestTypeValidator,
+            createRequestType);
+
+//delete an RequestType from the Account table
+router.delete("/deleteRequestType/:id");
+
+//update an RequestType in the Account table
+router.put("/updateRequestType/:Id",
+            RequestTypeValidator,
+            updateRequestType);
+
+module.exports = router;
