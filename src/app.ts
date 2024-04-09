e0b639da3d0e00a6ab61f2f243fe9a75fa67c5dc
Pastor José Jiménez Lucena <pastor.jimenez.0806@gmail.com>
2024-04-09 00:12:13 -0400

    swagger documentation

--- src/app.ts
+++ src/app.ts
@@ -2,12 +2,16 @@
 import express from 'express';
 import router from './routes/index'
 import cors from "cors";
+import swaggerUi from 'swagger-ui-express';
+import swaggerSetUp from "./routes/docs/swagger";
+
 
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSetUp));
 app.use('/api', router);
 
 export { app };
